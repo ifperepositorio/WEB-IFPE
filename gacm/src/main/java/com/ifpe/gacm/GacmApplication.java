4670bb5165c4b36710f8f31dd6d0508925d7c26e
Eduardo Alexandre <fabricadesoftware123@gmail.com>
2019-10-26 22:28:13 -0300

    Adicionando o Projeto

---
+++ gacm/src/main/java/com/ifpe/gacm/GacmApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.gacm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GacmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GacmApplication.class, args);
+	}
+
+}
