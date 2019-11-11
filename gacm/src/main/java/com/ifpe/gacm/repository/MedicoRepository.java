6b2d968ffd219e31c34e187fa46c6f1cc0794269
Eduardo Alexandre <fabricadesoftware123@gmail.com>
2019-11-11 16:49:36 -0300

    Sistema realizando missao 3

--- gacm/src/main/java/com/ifpe/gacm/repository/MedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifpe.gacm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ifpe.gacm.model.Medico;
-
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-
-}
